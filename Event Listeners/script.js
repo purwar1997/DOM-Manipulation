2df40918e18fd5305929890a5d9265da2c45cae5
purwar1997 <shubhampurwar511@gmail.com>
2023-02-01 12:47:41 +0530

    added some comments

--- Event Listeners/script.js
+++ Event Listeners/script.js
@@ -1,9 +1,10 @@
 // Syntax: element.addEventListener(event, callback);
+// This callback is known as event listener or event handler
 
 const btn2 = document.querySelector('.btn2');
 
 btn2.addEventListener('dblclick', function () {
-  alert('I also love JS');
+  alert('I also love JavaScript');
 });
 
 const btn3 = document.querySelector('.btn3');
